eac498613562d41f772e0baaa9885560fda6ea49
obregonia1 <n.y.skate.of.minds@gmail.com>
2021-02-25 12:20:10 +0900

    ユーザー一覧ページを実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/show'
   devise_for :users, controllers: {
     registrations: 'users/registrations',
     sessions: 'users/sessions'
   }
 
-  resources :books
+  resources :books, :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
