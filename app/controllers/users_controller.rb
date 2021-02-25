8d8eb82342c2a11d61137816876dab28baca6e5d
obregonia1 <n.y.skate.of.minds@gmail.com>
2021-02-25 18:49:23 +0900

    ユーザー一覧ページにページングを実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   before_action :set_user, only: :show
 
   def index
-    @users = User.all
+    @users = User.order(:id).page(params[:page]).per(3)
   end
 
   def show
