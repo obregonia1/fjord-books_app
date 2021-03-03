387283b6f4fef9006be43f5f71a85c9240e21edd
obregonia1 <n.y.skate.of.minds@gmail.com>
2021-03-04 08:23:09 +0900

    日本語訳を追加

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -5,11 +5,11 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_account_update_params, only: [:update]
 
   def after_sign_up_path_for(_resource)
-    '/books'
+    books_path
   end
 
   def after_update_path_for(_resource)
-    '/books'
+    books_path
   end
   # GET /resource/sign_up
   # def new
