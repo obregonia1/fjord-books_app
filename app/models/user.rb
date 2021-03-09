12f25f78f99c979b5c90855b0369e839fc4fc1d1
obregonia1 <n.y.skate.of.minds@gmail.com>
2021-03-09 16:52:08 +0900

    ユーザーフォローメソッドを実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,10 +6,14 @@ class User < ApplicationRecord
          :omniauthable, omniauth_providers: %i[github]
 
   has_one_attached :avatar
-  has_many :active_relationships, class_name:  "Relationship",
+  has_many :active_relationships, class_name:  'Relationship',
     foreign_key: 'follower_id',
     dependent: :destroy
-  has_many :following, through: :active_relationships, source: :following
+  has_many :passive_relationships, class_name:  'Relationship',
+           foreign_key: 'following_id',
+           dependent:   :destroy
+  has_many :followings, through: :active_relationships
+  has_many :followers, through: :passive_relationships
 
   validates :uid, uniqueness: { scope: :provider }, if: -> { uid.present? }
 
@@ -19,5 +23,17 @@ class User < ApplicationRecord
       user.email = auth.info.email
       user.password = Devise.friendly_token[0, 20]
     end
+  end
+
+  def follow(other_user)
+    followings << other_user
+  end
+
+  def unfollow(other_user)
+    active_relationships.find_by(following_id: other_user.id).destroy
+  end
+
+  def following?(other_user)
+    following.include?(other_user)
   end
 end
