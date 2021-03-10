b370e806b176f30933d55c1ec2cf8d46548390b5
obregonia1 <n.y.skate.of.minds@gmail.com>
2021-03-10 09:53:23 +0900

    各ユーザーのフォロー中、フォロワー一覧ページを実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,12 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  def followings
+    @users = User.order(:id).find(params[:id]).followings.page(params[:page])
+  end
+
+  def followers
+    @users = User.order(:id).find(params[:id]).followers.page(params[:page])
+  end
 end
