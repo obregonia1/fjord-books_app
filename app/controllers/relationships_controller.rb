77c7986d8796fa6f970cf0dc1e710157101e2227
obregonia1 <n.y.skate.of.minds@gmail.com>
2021-03-11 09:36:36 +0900

    rubocopに対応

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,15 +1,18 @@
+# frozen_string_literal: true
+
 class RelationshipsController < ApplicationController
   def create
     current_user.follow(user)
-    redirect_to users_path
+    redirect_to user_path(user)
   end
 
   def destroy
     current_user.unfollow(user)
-    redirect_to users_path
+    redirect_to user_path(user)
   end
 
   protected
+
   def user
     User.find(params[:relationship][:following_id])
   end
